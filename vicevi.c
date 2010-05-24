7d72ea82080fc629d82a56664b6f6327f404adc6
BKrsta <bkrsta@gmail.com>
2010-05-24 21:44:57 +0200

    Vicevi.c v1

--- vicevi.c
+++ vicevi.c
@@ -2,22 +2,15 @@
 #define MAXBRVICEVA 10
 
 char vicevi[MAXBRVICEVA][100];
-int  vicmax=0; // slobodnoMjestoZaVic - kod dodavanja novoga
-
-int brojviceva(){
-	int i, c=0;
-	for(i=0; i<MAXBRVICEVA; i++) if(vicevi[i]!="\0") c++;
-	return c;
-}
+int  vicmax=0;
 
 int main(void){
 
-	int i; // potrebna za 'for'
+	int i;
 	char odg;
 
 	ispis:
 
-	//for(i=0; i<MAXBRVICEVA; i++){
 	if(vicmax==0)
 		printf("Nema viceva u bazi. Dodaj novi!\n");
 	else
@@ -31,13 +24,14 @@ int main(void){
 	printf(" 1 - broj viceva\n");
 	printf(" 2 - ispis finti\n");
 	printf(" 3 - dodavanje fore\n");
+	printf(" 4 - KRAJ\n");
 	printf("====================\n");
 	printf(" > Choose your destiny: ");
 	scanf("%s", &odg);
-	printf("\n\n", odg);
+	printf("\n\n");
 
 	if(odg=='1'){
-		printf("Trenutni broj viceva jest %d\n", vicmax);
+		printf( (vicmax!=0)?"Trenutni broj viceva jest %d\n":"Nisi stavio viceva.\n", vicmax);
 	} else if(odg=='2'){
 		if(vicmax==0) printf("Pa konju! Nisi stavio ni jedan vic.");
 		printf("\nVicevi:\n");
@@ -49,13 +43,16 @@ int main(void){
 		scanf("%s", &vicevi[vicmax++]);
 		printf("stavio sam novi vic na mjesto %d\n", vicmax-1);
 		printf("... i nadam se da ima stila.");
+	} else if(odg=='4'){
+		printf(" ... Zbogom!\n");
+		system("PAUSE");
+		exit(0);
 	} else printf("\nA daj, bas si mastovit ...\n");
 
 	goto menu;
 
 	kraj:
-
-	// system("PAUSE");
+	system("PAUSE");
 	return 0;
 
 }
