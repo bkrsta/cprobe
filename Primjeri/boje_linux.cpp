53a69a7b5e42f98877adf461d52dbef03de8de6a
BKrsta <bkrsta@gmail.com>
2010-05-26 12:16:36 +0200

    Primjer boje_linux

---
+++ Primjeri/boje_linux.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	printf("Hello, world!\n");
+	printf("\033[22;31mHello, world!\n");
+	printf("\033[22;32mHello, world!\n");
+	printf("\033[22;33mHello, world!\n");
+	printf("\033[22;34mHello, world!\n");
+
+	return 0;
+}
